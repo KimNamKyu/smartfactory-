45157173f857e1e9808dde960c4f7d4f239d034a
김 남규 <skarb629@gmail.com>
2018-11-12 11:00:42 +0900

    20181112 디자인X 코드1

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -15,6 +15,26 @@ namespace WindowsFormsApp
         public Form1()
         {
             InitializeComponent();
+            Load += Form1_Load;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //button 클래스의 생성자 생성
+            Button btn = new Button();
+
+            //설정
+            btn.DialogResult = DialogResult.OK;
+            btn.Text = "확인";
+            btn.Size = new Size(100, 50);  //사이즈는 Size 생성자를 생성
+            btn.Location = new Point(30, 30);   //위치는 Point 생성자를 생성
+
+            Controls.Add(btn);  //화면 등록
+            btn.Click += btn_Click; //메소드를 연결 / += : 추가의 의미
+        }
+
+        private void btn_Click(object o, EventArgs args)
+        {
+            MessageBox.Show("클릭 메소드");
         }
     }
 }
