e6f5606af1c4c1cb38273910973c4686693733c0
김 남규 <skarb629@gmail.com>
2018-11-12 15:31:13 +0900

    ArrayList

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,29 +17,92 @@ namespace WindowsFormsApp
         {
             InitializeComponent();
             Load += Form1_Load;
-            CreateMyPanel();
+            //CreateMyPanel();
         }
 
         private Button btn;
         private void Form1_Load(object sender, EventArgs e)
         {
-            for(int i =0; i<3; i++) //버튼 3개 반복
+            //객체를 넣는방법
+            ArrayList arrList = new ArrayList();  //여러개 삽입가능 행추가가능.
+            arrList.Add(new Item("button",30,30,"btn_1"));   //object 형식은 무엇을 넣어도 상관없다.
+            arrList.Add(new Item("label", 30, 110, "lb_1"));
+            arrList.Add(new Item("button",30, 190, "btn_2"));
+
+            for(int i = 0; i<arrList.Count; i++)
             {
-                //button 클래스의 생성자 생성
-                btn = new Button();
-
-                //설정
-                btn.DialogResult = DialogResult.OK;
-                btn.Name = string.Format("btn_ : {0}", (i + 1));
-                btn.Text = string.Format("확인 : {0}" , (i+1));
-                btn.Size = new Size(100, 50);  //사이즈는 Size 생성자를 생성
-                btn.Location = new Point((100*i)+30, 30);  //x값 증가시켜 위치 변경
-                btn.Cursor = Cursors.Hand;  //커서 손모양 변경
-
-                Controls.Add(btn);  //화면 등록  ->> 여러개 넣었으므로 배열!
-                btn.Click += btn_Click; //메소드를 연결 / += : 추가의 의미
-
+               Control_create((Item)arrList[i]);
             }
+
+            /* 위에 나열한것과 동일한 방법 스트링을 넣은 방법
+            string[] ctrList = { "button", "label", "button" };
+
+            for (int i = 0; i < ctrList.Length; i++) //버튼 3개 반복
+            {
+                if (ctrList[i] == "button")
+                {
+                    Controls.Add(btn_create(i));
+                }
+                else if(ctrList[i] == "label")
+                {
+                    Controls.Add(lb_create(i));
+                }
+            }
+            */
+
+        }
+        private void Control_create(Item item)
+        {
+            Control ctr = new Control();
+            
+            //button or label 불러옴
+            switch (item.getType())
+            {
+                case "button":
+                    Button btn = new Button();
+                    btn.DialogResult = DialogResult.OK;
+                    btn.Cursor = Cursors.Hand;
+                    btn.Click += btn_Click;
+                    ctr = btn;
+                    break;
+                case "label":
+                    ctr = new Label();
+                    break;
+                default:
+                    break;
+            }
+            ctr.Name = item.getTxt();
+            ctr.Text = item.getTxt();
+            ctr.Size = new Size(100, 50); 
+            ctr.Location = new Point(item.getX(), item.getY());
+            Controls.Add(ctr);
+        }
+        private Button btn_create(int i)
+        {
+            //button 클래스의 생성자 생성
+            btn = new Button();
+
+            //설정
+            btn.DialogResult = DialogResult.OK;
+            btn.Name = string.Format("btn_ : {0}", (i + 1));
+            btn.Text = string.Format("확인 : {0}", (i + 1));
+            btn.Size = new Size(100, 50);  //사이즈는 Size 생성자를 생성
+            btn.Location = new Point((100 * i) + 30, 30);  //x값 증가시켜 위치 변경
+            btn.Cursor = Cursors.Hand;  //커서 손모양 변경
+            //Controls.Add(btn);  //화면 등록  ->> 여러개 넣었으므로 배열!
+            btn.Click += btn_Click; //메소드를 연결 / += : 추가의 의미
+            return btn;
+        }
+
+        private Label lb;
+        private Label lb_create(int i)
+        {
+            lb = new Label();
+            lb.Name = string.Format("lb_ : {0}", (i + 1));
+            lb.Text = string.Format("확인 : {0}", (i + 1));
+            lb.Size = new Size(100, 50);  
+            lb.Location = new Point((100 * i) + 30, 30);  
+            return lb;
         }
         private Panel panel1;
         private TextBox textBox1;
@@ -71,9 +135,10 @@ namespace WindowsFormsApp
                 // Add the Label and TextBox controls to the Panel.
                 panel1.Controls.Add(label1);
                 panel1.Controls.Add(textBox1);
+
+                //panel1.Click += ;
             }
         }
-
         private void btn_Click(object o, EventArgs args)
         {
             //string names = "";
@@ -99,6 +164,39 @@ namespace WindowsFormsApp
                 btn.BackColor = Color.Green;
             }
             */
+        }
+    }
+
+    public class Item
+    {
+        private string type; //button, label
+        private int X, Y;
+        private string txt;
+
+        public Item(string type,int X, int Y, string txt)
+        {
+            this.type = type;
+            this.X = X;
+            this.Y = Y;
+            this.txt = txt;
+        }
+
+        //public 으로 데이터를 담고 있을 객체로 가져다 사용 가능
+        public string getType()
+        {
+            return type;
+        }
+        public int getX()
+        {
+            return X;
+        }
+        public int getY()
+        {
+            return Y;
+        }
+        public string getTxt()
+        {
+            return txt;
         }
     }
 }
