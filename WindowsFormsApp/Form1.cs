53d0e9164e9c02750bf2c3d23465c6d4773d7c54
김 남규 <skarb629@gmail.com>
2018-11-12 11:17:50 +0900

    button 여러개 생성 하기

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -17,24 +17,45 @@ namespace WindowsFormsApp
             InitializeComponent();
             Load += Form1_Load;
         }
+        private Button btn;
         private void Form1_Load(object sender, EventArgs e)
         {
-            //button 클래스의 생성자 생성
-            Button btn = new Button();
+            for(int i =0; i<3; i++)
+            {
+                //button 클래스의 생성자 생성
+                Button btn = new Button();
 
-            //설정
-            btn.DialogResult = DialogResult.OK;
-            btn.Text = "확인";
-            btn.Size = new Size(100, 50);  //사이즈는 Size 생성자를 생성
-            btn.Location = new Point(30, 30);   //위치는 Point 생성자를 생성
+                //설정
+                btn.DialogResult = DialogResult.OK;
+                btn.Text = string.Format("확인 : {0}" , (i+1));
+                btn.Size = new Size(100, 50);  //사이즈는 Size 생성자를 생성
+                btn.Location = new Point((100*i)+30, 30);  //x값 증가시켜 위치 변경
+                btn.Cursor = Cursors.Hand;
 
-            Controls.Add(btn);  //화면 등록
-            btn.Click += btn_Click; //메소드를 연결 / += : 추가의 의미
+                Controls.Add(btn);  //화면 등록
+                btn.Click += btn_Click; //메소드를 연결 / += : 추가의 의미
+
+            }
         }
 
         private void btn_Click(object o, EventArgs args)
         {
-            MessageBox.Show("클릭 메소드");
+            btn = (Button)o;  //o는 객체이므로 형변환을 해주어야함
+            //MessageBox.Show(btn.Text);
+            //색깔넣기
+            btn.BackColor = (btn.BackColor == Color.Green ? btn.BackColor = Color.Silver : btn.BackColor = Color.Green);
+
+            /*
+            if(btn.BackColor == Color.Green)
+            {
+                btn.BackColor = Color.Silver;
+            }
+            else
+            {
+                btn.BackColor = Color.Green;
+            }
+            */
+            
         }
     }
 }
