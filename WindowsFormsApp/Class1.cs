b71c1d3488b113df71c8c34c0689723e19fe2f55
김 남규 <skarb629@gmail.com>
2018-11-14 12:03:12 +0900

    공통 모듈 개발

---
+++ WindowsFormsApp/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 11월14일 수업내용
+namespace WindowsFormsApp
+{
+    class Class1 
+    {
+        public void btn(btnobject bb)   
+        {
+            Button btn = new Button();
+            btn.DialogResult = DialogResult.OK;
+            btn.Name = bb.Name;
+            btn.Text = bb.Text;
+            btn.Size = new Size(bb.SX, bb.SY);  
+            btn.Location = new Point(bb.PX, bb.PY); 
+            btn.Cursor = Cursors.Hand;
+            bb.Form.Controls.Add(btn);
+        }
+
+        public void lb(lbobject lb)
+        {
+            Label label = new Label();
+            label.Name = lb.Name;
+            label.Text = lb.Text;
+            label.Size = new Size(lb.SX, lb.SY);
+            label.Location = new Point(lb.PX, lb.PY);
+            label.Cursor = Cursors.Hand;
+            lb.Form.Controls.Add(label);
+        }
+    }
+}
---
+++ WindowsFormsApp/Form4.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+            Load += Form4_Load; //폼이 시작되기 전에 동작
+        }
+        private void Form4_Load(object sender, EventArgs args)  
+        {
+            //Button btn = c1.btn(); 
+            //Controls.Add(btn);
+
+            //Class1 c1 = new Class1(this);
+            //c1.btn();
+            
+            Class1 c1 = new Class1();
+            //c1.btn(this, "btn_1", "버튼1", 100, 50, 30, 30);
+            //c1.btn(this, "btn_1", "버튼1", 100, 50, 30, (30 + 30 + 50));
+            ArrayList arr = new ArrayList();
+            arr.Add(new btnobject(this, "btn_1", "버튼1", 100, 50, 30, 30));
+            arr.Add(new btnobject(this, "btn_1", "버튼1", 100, 50, 30, (30 + 30 + 50)));
+            arr.Add(new lbobject(this, "lb", "라벨1", 100, 50, (30+30+100), 30));
+
+            for (int i = 0; i<arr.Count; i++)
+            {
+                //제어문 사용하여 type비교
+                if (typeof(btnobject) == arr[i].GetType())  //arr[i] 객체가 btn의 객체이면 (type을 비교)
+                {
+                    c1.btn((btnobject)arr[i]);  
+                }
+                else if (typeof(lbobject) == arr[i].GetType()) //arr[i] 객체가 lb (type 비교)
+                {
+                    c1.lb((lbobject)arr[i]);
+                }
+            }
+        }
+    }
+}
