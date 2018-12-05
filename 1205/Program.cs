3139f0e23703f071993627fc189c28ffdbdea376
김 남규 <skarb629@gmail.com>
2018-12-05 17:06:52 +0900

    1205수업

---
+++ 1205/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1205
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient(); //객체생성
+            NameValueCollection data = new NameValueCollection();  //key / value 형식으로 받음
+            //헤더에 담아서 추가.(웹 규약)
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            client.Encoding = Encoding.UTF8;    //한글
+
+            string url = "http://192.168.3.10:5000/api/Insert";
+            string method = "POST";
+            
+            data.Add("id", "1");
+            data.Add("name", "테스트");
+            data.Add("passwd", "1234");
+
+            byte[] result = client.UploadValues(url,method,data);
+            string strResult = Encoding.UTF8.GetString(result);
+            //Console.WriteLine(strResult);
+            //List<object> list = new List<object>();
+            //list.Add();
+            //Console.WriteLine(list);
+              
+            //string 을 ArrayList로 변환 해줘야함 > ArrayList list = (ArrayList)result; string을 ArrayList로 담는건 불가능!
+
+            ArrayList list = new ArrayList(result);
+            list.Add(strResult[0]);
+            
+            
+        }
+    }
+}
--- 1205/Properties/AssemblyInfo.cs
+++ 1205/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("1121")]
+[assembly: AssemblyTitle("1205")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("1121")]
+[assembly: AssemblyProduct("1205")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("d8b535c0-93c6-483e-bae0-2fe382cc8a37")]
+[assembly: Guid("c2036182-dbb2-4c35-b30c-32aca0623653")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
